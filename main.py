741d4e7125961957440195e0ec7c0e92048e2b60
ilhamshodiq <ilhambheh@gmail.com>
2022-04-02 16:48:14 +0700

    Nambah command cuaca

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ import datetime
 import pytz
 import youtube_dl
 import pafy
+import requests
 
-wib = pytz.timezone('Asia/Jakarta') 
+# APInya weathermap
+api_key = "f55290c77e1a327777c1a4ed66896ef9"  # pake keymu sendiri lah..
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+# timezone wib
+wib = pytz.timezone('Asia/Jakarta')
 current_time = datetime.datetime.now(wib)
 
-
 # client = discord.Client() # not used
-commands = commands.Bot(command_prefix='')#make command tapi gamake :v
+commands = commands.Bot(command_prefix='')  # make command tapi gamake :v
 
 f = open('pesan.txt', 'r')
 file_contents = f.read()
@@ -24,10 +29,50 @@ file_commands = f2.read()
 f3 = open('commands2.txt', 'r')
 file_commands2 = f3.read()
 
+
 @commands.event
 async def on_ready():
-    await commands.change_presence(activity=discord.Game(name="DARK SOULS III"))
+    await commands.change_presence(activity=discord.Game(name="ELDEN RING")
+                                   )
     print('Masuk dengan {0.user}'.format(commands))
+
+# command cuaca
+
+
+@commands.command()
+async def cuaca(ctx, *, city: str):
+    city_name = city
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+    channel = ctx.message.channel
+    if x["cod"] != "404":
+        async with channel.typing():
+            y = x["main"]
+            current_temperature = y["temp"]
+            current_temperature_celsiuis = str(
+                round(current_temperature - 273.15))
+            current_pressure = y["pressure"]
+            current_humidity = y["humidity"]
+            z = x["weather"]
+            weather_description = z[0]["description"]
+            embed = discord.Embed(title=f"Cuaca di {city_name}",
+                                  color=ctx.guild.me.top_role.color,
+                                  timestamp=ctx.message.created_at,)
+            embed.add_field(name="Deskripsi",
+                            value=f"**{weather_description}**", inline=False)
+            embed.add_field(
+                name="Temperatur(C)", value=f"**{current_temperature_celsiuis}°C**", inline=False)
+            embed.add_field(name="Kelembapan(%)",
+                            value=f"**{current_humidity}%**", inline=False)
+            embed.add_field(name="Tekanan Atmosfir(hPa)",
+                            value=f"**{current_pressure}hPa**", inline=False)
+            embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
+            embed.set_footer(text=f"Requested by {ctx.author.name}")
+            await channel.send(embed=embed)
+    else:
+        await channel.send("City not found.")
+
 
 @commands.command()
 async def katakan(ctx, *, question):
@@ -36,19 +81,55 @@ async def katakan(ctx, *, question):
 
     await ctx.send(f"{question}")
 
+
 @commands.command()
 async def p(ctx):
     await ctx.send('{} ms'.format(round(commands.latency * 1000)))
 
 
-#show avatar
+# userinfo
+@commands.command()
+async def userinfo(ctx, member: discord.Member = None):
+    await ctx.message.delete()
+    if not member:
+        member = ctx.message.author
+    roles = ([role for role in member.roles[1:]])
+    embed = discord.Embed(colour=discord.Colour.random(),
+                          title=f"User Info - {member}")
+    embed.set_thumbnail(url=member.avatar_url)
+
+    embed.add_field(name="ID:", value=member.id)
+    embed.add_field(name="User Name:", value=member.display_name)
+
+    embed.add_field(
+        name="Created Account On:",
+        value=member.created_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
+    embed.add_field(
+        name="Joined Server On:",
+        value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC") +
+        "\u0020")
+
+    if roles == []:
+        embed.add_field(name="Roles:", value="None")
+        embed.add_field(name="Highest Role:", value="None")
+        await ctx.send(embed=embed)
+
+    else:
+        embed.add_field(name="Roles:",
+                        value=", ".join([role.mention for role in roles]))
+        embed.add_field(name="Highest Role:", value=member.top_role.mention)
+        await ctx.send(embed=embed)
+
+
+# show avatar
 @commands.command()
 async def avatar(ctx, member: discord.Member):
     show_avatar = discord.Embed(color=discord.Color.dark_purple())
     show_avatar.set_image(url='{}'.format(member.avatar_url))
     await ctx.send(embed=show_avatar)
 
-#download yt mp3
+
+# download yt mp3
 @commands.command()
 async def download_mp3(ctx, *, link):
     await ctx.reply('Oke tak download sek..')
@@ -61,16 +142,21 @@ async def download_mp3(ctx, *, link):
     await ctx.channel.send(file=discord.File(judul))
     os.remove(judul)
 
-#alarm
+
+# alarm
 @commands.command()
 async def ingatkan(ctx, time, *, ingatkan):
     print(time)
     print(ingatkan)
     user = ctx.message.author
-    embed = discord.Embed(color=discord.Color.dark_purple(), timestamp=datetime.utcnow())
+    embed = discord.Embed(color=discord.Color.dark_purple(),
+                          timestamp=datetime.datetime.now())
     seconds = 0
     if ingatkan is None:
-        embed.add_field(name='Warning', value='Mau minta ingatkan apa bro? yang bener nulisnya.') # Error message
+        embed.add_field(
+            name='Warning',
+            value='Mau minta ingatkan apa bro? yang bener nulisnya.'
+        )  # Error message
     if time.lower().endswith("hari"):
         seconds += int(time[:-4]) * 60 * 60 * 24
         counter = f"{seconds // 60 // 60 // 24} hari"
@@ -90,14 +176,17 @@ async def ingatkan(ctx, time, *, ingatkan):
     else:
         await ctx.send(f"Okeee, ntar aku ingetin {ingatkan}, {counter} lagi.")
         await asyncio.sleep(seconds)
-        await ctx.reply(f"Ninu ninu!!🔔 tadi {counter} yang lalu, kamu minta ingetin '{ingatkan}'")
+        await ctx.reply(
+            f"Ninu ninu!!🔔 tadi {counter} yang lalu, kamu minta ingetin '{ingatkan}'"
+        )
         return
     await ctx.send(embed=embed)
 
-#pake event
+
+# pake event
 @commands.event
 async def on_message(message):
-    #input dijadiin lower & hilangin spasi
+    # input dijadiin lower & hilangin spasi
     pesankeciltanpaspasi = message.content.lower().replace(' ', '')
     pesankecil = message.content.lower()
 
@@ -117,7 +206,7 @@ async def on_message(message):
         await message.channel.send('Alhamdulillah sehat')
 
     if pesankeciltanpaspasi.startswith('goodnight'):
-      await message.channel.send(file_contents)      
+        await message.channel.send(file_contents)
 
     if pesankecil.startswith('pilih'):
         pilihan = pesankecil
@@ -137,61 +226,68 @@ async def on_message(message):
 
     if pesankecil.startswith('dor'):
         await message.channel.send('kaget wooyy')
-    
+
     if pesankecil.startswith('😳'):
-        await message.reply('😳👉👈')        
+        await message.reply('😳👉👈')
+
+    if pesankecil.startswith('-20'):
+        await message.reply(
+            'https://cdn.discordapp.com/attachments/624472802208251946/916660131268288543/-20.png'
+        )
 
     if pesankecil.startswith('jam berapa sekarang?'):
-        embed = discord.Embed(color=discord.Color.dark_purple(), timestamp=datetime.utcnow())
-        embed.add_field(name='🕓' ,value='Jam 👇')
+        embed = discord.Embed(color=discord.Color.dark_purple(),
+                              timestamp=datetime.utcnow())
+        embed.add_field(name='🕓', value='Jam 👇')
         await message.channel.send(embed=embed)
-    
+
     #greet in time
     if pesankeciltanpaspasi.startswith('selamatsiang'):
-       jamsekarang = current_time.hour
-       pesan = ''
-       if jamsekarang >= 11 and jamsekarang < 17:
-          pesan = 'Siang 🌞'      
-       elif jamsekarang >= 17 or jamsekarang < 3:
-          pesan = 'Udah malem wooyy'
-       elif jamsekarang >= 3 and jamsekarang < 11:
-          pesan = 'Masih pagi heh'      
-       await message.channel.send(pesan)
+        jamsekarang = current_time.hour
+        pesan = ''
+        if jamsekarang >= 11 and jamsekarang < 17:
+            pesan = 'Siang 🌞'
+        elif jamsekarang >= 17 or jamsekarang < 3:
+            pesan = 'Udah malem wooyy'
+        elif jamsekarang >= 3 and jamsekarang < 11:
+            pesan = 'Masih pagi heh'
+        await message.channel.send(pesan)
     if pesankeciltanpaspasi.startswith('selamatmalam'):
-       jamsekarang = current_time.hour
-       pesan = ''
-       if jamsekarang >= 11 and jamsekarang < 17:
-          pesan = 'Masih siang wooyyy 🌞'      
-       elif jamsekarang >= 17 or jamsekarang < 3:
-          pesan = 'Malemmmm 😴'
-       elif jamsekarang >= 3 and jamsekarang < 11:
-          pesan = 'Masih pagi heh'      
-       await message.channel.send(pesan)
+        jamsekarang = current_time.hour
+        pesan = ''
+        if jamsekarang >= 11 and jamsekarang < 17:
+            pesan = 'Masih siang wooyyy 🌞'
+        elif jamsekarang >= 17 or jamsekarang < 3:
+            pesan = 'Malemmmm 😴'
+        elif jamsekarang >= 3 and jamsekarang < 11:
+            pesan = 'Masih pagi heh'
+        await message.channel.send(pesan)
 
     if pesankeciltanpaspasi.startswith('selamatpagi'):
-       jamsekarang = current_time.hour
-       pesan = ''
-       if jamsekarang >= 11 and jamsekarang < 17:
-          pesan = 'udah siang wooyyy 🌞'      
-       elif jamsekarang >= 17 or jamsekarang < 3:
-          pesan = 'malem sek'
-       elif jamsekarang >= 3 and jamsekarang < 11:
-          pesan = 'Pagi............ udah sarapan?'      
-       await message.channel.send(pesan)
+        jamsekarang = current_time.hour
+        pesan = ''
+        if jamsekarang >= 11 and jamsekarang < 17:
+            pesan = 'udah siang wooyyy 🌞'
+        elif jamsekarang >= 17 or jamsekarang < 3:
+            pesan = 'malem sek'
+        elif jamsekarang >= 3 and jamsekarang < 11:
+            pesan = 'Pagi............ udah sarapan?'
+        await message.channel.send(pesan)
     #############################
 
-    #googlekan
+    # googlekan
     if pesankecil.startswith('tolong googlekan'):
-       masukan = pesankecil
-       masukan = masukan.replace('tolong googlekan ', '').replace(' ', '+')
-       await message.reply('https://letmegooglethat.com/?q={}'.format(masukan))    
+        masukan = pesankecil
+        masukan = masukan.replace('tolong googlekan ', '').replace(' ', '+')
+        await message.reply('https://letmegooglethat.com/?q={}'.format(masukan)
+                            )
 
     if pesankecil.startswith('--help'):
-       embed = discord.Embed(color=discord.Color.dark_purple())       
-       embed.add_field(name = '**~DAFTAR COMMANDS~**', value = file_commands)
-       embed.add_field(name = '============', value = file_commands2)
-       embed.set_footer(icon_url = commands.user.avatar_url, text = 'commands')
-       await message.channel.send(embed = embed)
+        embed = discord.Embed(color=discord.Color.dark_purple())
+        embed.add_field(name='**~DAFTAR COMMANDS~**', value=file_commands)
+        embed.add_field(name='============', value=file_commands2)
+        embed.set_footer(icon_url=commands.user.avatar_url, text='commands')
+        await message.channel.send(embed=embed)
 
     await commands.process_commands(message)
 
